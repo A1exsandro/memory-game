15b5decba67d9e0ed9c21bfc0797bd71731f3884
A1exsandro <alexsandro.acp@gmail.com>
2022-07-19 16:55:18 -0400

    data and utils

---
+++ src/data/cards.ts
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    id: '1',
+    back: 'First'
+  },
+  {
+    id: '2',
+    back: 'Love'
+  },
+  {
+    id: '3',
+    back: 'Try'
+  },
+  {
+    id: '4',
+    back: 'Home'
+  },
+  {
+    id: '5',
+    back: 'Game'
+  },
+  {
+    id: '6',
+    back: 'You'
+  },
+];
