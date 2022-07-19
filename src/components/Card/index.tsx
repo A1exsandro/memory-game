5ef6c1b0fa184af4b1649ce5f7fe2b9bfb6627fe
A1exsandro <alexsandro.acp@gmail.com>
2022-07-19 16:54:15 -0400

    add component Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+
+export interface CardProps {
+  id: string;
+  flipped?: boolean;
+  back: string;
+  handleClick?: (id: string) => void;
+}
+
+export function Card({ flipped = false, back, handleClick, id }: CardProps) {
+  const cardContentClassName = ['card_content'];
+  flipped && cardContentClassName.push('card_content--flipped');
+
+  const handleClickfn = (id: string) => {
+    if(handleClick) {
+      handleClick(id);
+    }
+  }
+
+  return(
+    <div className="card" onClick={() => handleClickfn(id)}>
+      <div className={cardContentClassName.join('')}>
+        <div className="card_face card_face--front">Front</div>
+        <div className="card_face card_face--back">{back}</div>
+      </div>
+    </div>
+  );
+}
